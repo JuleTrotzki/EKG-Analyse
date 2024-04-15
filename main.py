8fe2d4f1af2da9a0055784d2ba93b23834fa3007
JuleTrotzki <tj4062@mci4me.at>
2024-04-15 16:10:57 +0200

    namenliste erstellen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import read_data # Ergänzen Ihr eigenes Modul
 
 # Eine Überschrift der ersten Ebene
 st.write("# EKG APP")
@@ -6,7 +7,15 @@ st.write("# EKG APP")
 # Eine Überschrift der zweiten Ebene
 st.write("## Versuchsperson auswählen")
 
-# Eine Auswahlbox
-current_user = st.selectbox(
+
+# Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre 
+# Funktionen aufrufen
+person_dict = read_data.load_person_data()
+person_names = read_data.get_person_list(person_dict)
+# bzw: wenn Sie nicht zwei separate Funktionen haben
+#person_names = read_data.get_person_list()
+
+# Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
+st.session_state.current_user = st.selectbox(
     'Versuchsperson',
-    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
+    options = person_names)
--- read_data.py
+++ read_data.py
@@ -1,9 +1,21 @@
 import json
 
-# Opening JSON file
-file = open("data/person_db.json")
+def load_person_data():
+    """A Function that knows where te person Database is and returns a Dictionary with the Persons"""
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    return person_data
 
-# Loading the JSON File in a dictionary
-person_data = json.load(file)
+person_data = load_person_data()
+print(person_data)
 
-person_data
+
+def get_person_list(person_data):
+    namen = []
+    for person in person_data:
+        name = f"{person['firstname']} {person['lastname']}"
+        namen.append(name)
+    return namen
+
+namen = get_person_list(person_data)
+print(namen)
